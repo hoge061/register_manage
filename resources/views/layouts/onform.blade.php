800cea7ec7ddcccf092f6ee8a3f07072ba7ddc33
ren <hoge061@gmail.com>
2023-10-20 14:32:56 +0900

    a

--- resources/views/layouts/onform.blade.php
+++ resources/views/layouts/onform.blade.php
@@ -2,6 +2,7 @@
 <html lang="ja">
 <head>
     @livewireStyles
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 <body class="antialiased overflow-x-hidden">
 
--- resources/views/main/on_register.blade.php
+++ resources/views/main/on_register.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.hpcreate003')
-
+@section('head')
+<link rel="stylesheet" href="{{ asset('css/style.css') }}">
+@endsection
 @section('main')
 オンライン登録ページ
 {{-- @livewire('counter') --}}
